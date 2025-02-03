568dcf5655d7b7d47658bdf7d971bd2d9b981ac9
opuebla56 <pueblao@ferris.edu>
2025-02-03 17:27:06 -0500

    moved method to rental class

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -13,6 +13,26 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
+    static double getChargeFor(Rental rental) {
+        double amount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (rental.getDaysRented() > 2)
+                    amount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    amount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
     public Movie getMovie() {
         return movie;
     }
